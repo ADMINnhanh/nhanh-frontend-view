cef6deb81b187d55fef9d095d91078a236d0e1bb
nhanh <3201595386@qq.com>
2024-10-22 17:38:38 +0800

    更换为 哈希 路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 import home from "../views/home.vue";
 import Routes from "./Routes";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
