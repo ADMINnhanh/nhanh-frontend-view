7a31b96b48f29eb332ddd098f05dba1353232d27
nhanh <3201595386@qq.com>
2024-10-30 09:27:58 +0800

    尝试修复 github action 打包异常

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      // "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@": path.resolve(__dirname, "src"),
     },
   },
@@ -19,5 +15,8 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     cors: true, // 允许跨域
+  },
+  build: {
+    outDir: "dist",
   },
 });
