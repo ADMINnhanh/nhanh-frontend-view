62da0bb5cc518fcecc2e800832618153b96aa2e8
ADMINnhanh <59279426+ADMINnhanh@users.noreply.github.com>
2026-02-26 06:46:00 +0000

    Deploying to gh-pages from @ ADMINnhanh/nhanh-frontend-view@390e942f6433ca7a60dd5b0a1183f0e99672b40f 🚀

--- assets/use-locale-DjTo9B-1.js
+++
@@ -1,1 +0,0 @@
-import{f as r,g as d,d5 as u,d6 as c,h as i}from"./index-DiLUuH_l.js";function f(n){const{mergedLocaleRef:o,mergedDateLocaleRef:a}=r(i,null)||{},t=d(()=>{var e,l;return(l=(e=o==null?void 0:o.value)===null||e===void 0?void 0:e[n])!==null&&l!==void 0?l:u[n]});return{dateLocaleRef:d(()=>{var e;return(e=a==null?void 0:a.value)!==null&&e!==void 0?e:c}),localeRef:t}}export{f as u};
