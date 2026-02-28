8f282df9b8515a44366fedde056301457543dd64
ADMINnhanh <59279426+ADMINnhanh@users.noreply.github.com>
2026-02-28 01:03:48 +0000

    Deploying to gh-pages from @ ADMINnhanh/nhanh-frontend-view@9981c87fa732fd93a2be63e020fc8b5fe5686d32 🚀

---
+++ assets/a-0G5CvdAO.js
@@ -0,0 +1,8 @@
+import{j as c,d,l as m,m as h,L as r,d3 as u,g as f,N as v}from"./index-C79Lo4xg.js";const p=c("a",`
+ cursor: pointer;
+ transition:
+ color .3s var(--n-bezier),
+ text-decoration-color .3s var(--n-bezier);
+ text-decoration-color: var(--n-text-color);
+ color: var(--n-text-color);
+`),C=Object.assign({},r.props),g=d({name:"A",props:C,setup(e){const{mergedClsPrefixRef:o,inlineThemeDisabled:t}=h(e),a=r("Typography","-a",p,u,e,o),n=f(()=>{const{common:{cubicBezierEaseInOut:i},self:{aTextColor:l}}=a.value;return{"--n-text-color":l,"--n-bezier":i}}),s=t?v("a",void 0,n,e):void 0;return{mergedClsPrefix:o,cssVars:t?void 0:n,themeClass:s==null?void 0:s.themeClass,onRender:s==null?void 0:s.onRender}},render(){var e;return(e=this.onRender)===null||e===void 0||e.call(this),m("a",{class:[`${this.mergedClsPrefix}-a`,this.themeClass],style:this.cssVars},this.$slots)}});export{g as N};
